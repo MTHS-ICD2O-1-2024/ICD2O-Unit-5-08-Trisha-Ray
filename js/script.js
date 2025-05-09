9c67a2be72b95f4ca66eb00bef0c734d779439c2
default <trisha.ray@stu.ocsb.ca>
2025-05-09 16:51:24 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Trisha Ray
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+'use strict'
+
+function calculate() {
+  // input
+  const movieRating = document.getElementById("movieforage").value
+
+  // process
+  if (movieRating >= 17) {
+    // output
+    document.getElementById("results").innerHTML =
+      "You can see R rated movies."
+  }
+
+  else if (movieRating >= 13) {
+    document.getElementById("results").innerHTML =
+      "You can see PG-13 rated movies."
+  }
+
+  else if (movieRating >= 5) {
+    document.getElementById("results").innerHTML =
+      "You can see G and PG rated movies."
+  }
+
+  else {
+    document.getElementById("results").innerHTML =
+      "Uhh, you are to young to see most things."
+  }
+}
